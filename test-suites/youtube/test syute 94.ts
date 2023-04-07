eadacacbe9c347767bc33fc9635d41a5a8908367
laiphamkat <116550309+laiphamkat@users.noreply.github.com>
2023-04-07 16:36:12 +0700

    test

---
+++ test-suites/youtube/test syute 94.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>test syute 94</name>
+    <testSuiteGuid>d039ef3b-52ef-43bd-a4ba-86078056516c</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/New test case</testCaseId>
+        <guid>c21b0d48-a1b5-45e7-a063-4910db7a4a02</guid>
+    </testCaseLink>
+</TestSuiteEntity>
