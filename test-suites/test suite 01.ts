a8b9ce17b24191f9f44a3228aecbc0ecd7abf44a
laiphamkat <116550309+laiphamkat@users.noreply.github.com>
2023-04-07 11:38:47 +0700

    test

---
+++ test-suites/test suite 01.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>test suite 01</name>
+    <testSuiteGuid>2e7abe41-3ebd-4635-8d9c-9d51a76c3429</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/New test case</testCaseId>
+        <guid>bb3c393d-c720-4595-ae33-9ab94ec12988</guid>
+    </testCaseLink>
+</TestSuiteEntity>
