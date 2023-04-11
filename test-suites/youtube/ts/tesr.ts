e593e93f68750afcffc7c4b9381d769bed14e389
laiphamkat <116550309+laiphamkat@users.noreply.github.com>
2023-04-11 15:52:47 +0700

    test

---
+++ test-suites/youtube/ts/tesr.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>tesr</name>
+    <testSuiteGuid>d8abffad-b7b8-47d9-b375-3b5d75eec123</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/Test 02</testCaseId>
+        <guid>75c12e65-eeb1-4f09-96e1-374e3a690cfa</guid>
+    </testCaseLink>
+</TestSuiteEntity>
