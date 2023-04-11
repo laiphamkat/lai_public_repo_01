6afba2d79eda4942b62ff8467c7808e6ed48962e
laiphamkat <116550309+laiphamkat@users.noreply.github.com>
2023-04-11 15:56:40 +0700

    m

---
+++ test-suites/youtube/ts/n.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>n</name>
+    <testSuiteGuid>eb98eb17-e593-4bfe-84e6-ba3dc52a8d12</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/New test case</testCaseId>
+        <guid>7890ca19-699e-4a59-83f6-24bf9e19bbf9</guid>
+    </testCaseLink>
+</TestSuiteEntity>
