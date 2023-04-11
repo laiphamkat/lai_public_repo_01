e60d8058675c4f9efb70e586f77fd5aec301bad1
laiphamkat <116550309+laiphamkat@users.noreply.github.com>
2023-04-11 15:44:38 +0700

    test

---
+++ test-suites/youtube/Test Suite 001.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>Test Suite 001</name>
+    <testSuiteGuid>dd8a0e71-f4cc-4cc3-955f-d5858cfb65a8</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/New test case</testCaseId>
+        <guid>8cd064e2-016b-4824-8552-f34ff755c1b0</guid>
+    </testCaseLink>
+</TestSuiteEntity>
