7c6fbdffd48117ed708b08e4cd43c6b9cbe0ea5a
laiphamkat <116550309+laiphamkat@users.noreply.github.com>
2023-04-11 15:45:05 +0700

    Katalon: Update test suite Test Suite 001 in test-suites/youtube

--- test-suites/youtube/Test Suite 001.ts
+++ test-suites/youtube/Test Suite 001.ts
@@ -1,9 +1,17 @@
 <?xml version="1.0" encoding="UTF-8" standalone="yes"?>
 <TestSuiteEntity>
     <name>Test Suite 001</name>
-    <testSuiteGuid>dd8a0e71-f4cc-4cc3-955f-d5858cfb65a8</testSuiteGuid>
+    <testSuiteGuid>b0ea4eab-5a0d-45fb-a9bb-e2fea3f3455f</testSuiteGuid>
     <testCaseLink>
         <testCaseId>test-cases/New test case</testCaseId>
-        <guid>8cd064e2-016b-4824-8552-f34ff755c1b0</guid>
+        <guid>d306e234-8598-474a-8fe2-9c991b3c3353</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/New test case</testCaseId>
+        <guid>70fa00ac-9927-49ff-b8a3-68443a69410c</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/Test 02</testCaseId>
+        <guid>b0ad25f9-b9cb-427a-81e9-410a304fa8f7</guid>
     </testCaseLink>
 </TestSuiteEntity>
